c52f0867636c83216b9dc87037d3d8e1373a333b
CheolyongKim <chdragon01@hanyang.ac.kr>
2026-06-14 11:42:21 +0900

    [Wrong Answer] 최단 Run Length 인코딩 (2ms, 0MB)

---
+++ trail4/최단 Run Length 인코딩/shortest-run-length-encoding.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// def
+string A;
+int minimum;
+
+void rshift(){
+    char tail = A[A.length() - 1];
+    for (int i=A.length()-1; i>0; i--) A[i]=A[i-1];
+    A[0]=tail;
+}
+
+int RLE(string A){
+    int rle = 0;
+
+    while (A.length()>0){
+        char head = A.at(0);
+        while (!A.empty() && head == A.at(0)) {
+            A.erase(0,1);
+        }
+        rle += 2;
+    }
+    return rle;
+}
+
+void input(){
+    cin >> A;
+}
+
+void process(){
+    minimum = A.length();
+
+    // x*10
+    bool flag = true;
+    for (int i=1; i<A.length(); i++)
+        if (A[i]!=A[i-1]) flag = false;
+    if (flag){
+        minimum = 3;
+        return;
+    }
+
+    // process
+    for (int i=0; i<A.length(); i++){
+        minimum = min(minimum, RLE(A));
+        rshift();
+    }
+}
+
+void output(){
+    cout << minimum;
+}
+
+int main() {
+    input();
+    process();
+    output();
+    return 0;
+}
